4f78e75e35aae851c547090acb0f12b1b28c46f8
Aman dhoni <parleaman@gmail.com>
2025-08-28 16:20:58 +0530

    smallest and largset no

--- array/Largest.java
+++ array/Largest.java
@@ -2,11 +2,16 @@
 public class Largest {
     public static int largest(int arr[]){
         int maxNumber=Integer.MIN_VALUE;
+        int smallNumber=Integer.MAX_VALUE;
         for(int i=0; i < arr.length; i++){
             if(maxNumber < arr[i]){
                 maxNumber=arr[i];
             }
+            if(smallNumber > arr[i]){
+                smallNumber=arr[i];
+            }
         }
+        System.out.println("Smallest value is :"+smallNumber);
         return maxNumber;
     }
     public static void main(String[] args) {
