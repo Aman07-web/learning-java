2f244ebf3d2923d6ea85e6d393192b94d39a8fe5
Aman dhoni <parleaman@gmail.com>
2025-09-01 21:27:14 +0530

    commit

--- Assignments/FabSeries.java
+++ Assignments/FabSeries.java
@@ -1,5 +1,17 @@
-public class FabSeries {
+import java.util.Scanner;
+
+public class FabSeries{
     public static void main(String[] args) {
-        
-    } 
+        System.out.println("Enter range of Fabonacci series: ");
+        Scanner sc=new Scanner(System.in);
+        int range=sc.nextInt();
+        int a=0,b=1,c;
+         for(int i=1; i <=range; i++) {
+            System.out.print(a+" ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
 }
+
