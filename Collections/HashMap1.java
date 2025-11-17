9da28c44fd0d4cb95e252644288b5289f1312398
Aman dhoni <parleaman@gmail.com>
2025-11-17 08:32:15 +0530

    map

---
+++ Collections/HashMap1.java
@@ -0,0 +1,10 @@
+package Collections;
+import java.util.*;;
+public class HashMap1 {
+    public static void main(String[] args) {
+        Map <Integer, Character> map=new HashMap<>();
+        map.put(1, 'A');
+        map.put(2, 'M');
+        System.out.println(map);
+    }
+}
