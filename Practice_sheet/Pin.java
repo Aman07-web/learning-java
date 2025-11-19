0e730388b3033efbc9d176b56387ffe8509e1d48
Aman dhoni <parleaman@gmail.com>
2025-11-19 09:19:34 +0530

    pin generation

--- Practice_sheet/Pin.java
+++ Practice_sheet/Pin.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Pin {
     public static String generationPin(){
         Random rnd=new Random();
-        int number=rnd.nextInt(10000);
-        return String.format("%04sd%", number);
+        int number=rnd.nextInt(1000000);
+        return String.format("%06d", number);
     }
     public static void main(String[] args) {
         System.out.println("PIN : "+generationPin());
