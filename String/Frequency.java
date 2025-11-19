a0da82ac36dfcc4673bc41e61406f0e73e092ebd
Aman dhoni <parleaman@gmail.com>
2025-11-19 09:30:01 +0530

    frequency count

---
+++ String/Frequency.java
@@ -0,0 +1,22 @@
+package String;
+
+public class Frequency {
+    public static void main(String[] args) {
+       int nums=36234554;
+       int freq[]=new int[10];
+       while (nums > 0) {
+        int digits=nums%10;
+        freq[digits]++;
+        nums=nums/10;
+       }
+       System.out.println("Frequcies : ");
+       for(int i=0; i<10; i++){
+        if(freq[i] > 0){
+            System.out.println(i+" => "+freq[i]);
+        }
+       }
+       
+
+    }
+    
+}
